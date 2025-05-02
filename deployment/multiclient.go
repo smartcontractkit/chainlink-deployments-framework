85d2c0c8d1e37ed9c1b4c5f46fcbc8f03b612197
bytesizedroll <damien.hobday@smartcontract.com>
2025-05-02 16:21:10 -0400

    temp disable retry

--- deployment/multiclient.go
+++ deployment/multiclient.go
@@ -21,11 +21,11 @@ import (
 
 const (
 	// Default retry configuration for RPC calls
-	RPCDefaultRetryAttempts = 10
+	RPCDefaultRetryAttempts = 0
 	RPCDefaultRetryDelay    = 1000 * time.Millisecond
 
 	// Default retry configuration for dialing RPC endpoints
-	RPCDefaultDialRetryAttempts = 10
+	RPCDefaultDialRetryAttempts = 0
 	RPCDefaultDialRetryDelay    = 1000 * time.Millisecond
 )
 
