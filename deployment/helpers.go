8e845f1272ca084ef3036ef1fa9ad1be5a14c6b7
Ilan <36084092+ilanolkies@users.noreply.github.com>
2025-05-13 11:55:50 -0300

    Add ZK env (#79)
    Follows https://github.com/smartcontractkit/chainlink/pull/17436

--- deployment/helpers.go
+++ deployment/helpers.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
+
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 )
@@ -128,10 +129,13 @@ func DeployContract[C any](
 		lggr.Errorw("Failed to deploy contract", "chain", chain.String(), "err", contractDeploy.Err)
 		return nil, contractDeploy.Err
 	}
-	_, err := chain.Confirm(contractDeploy.Tx)
-	if err != nil {
-		lggr.Errorw("Failed to confirm deployment", "chain", chain.String(), "Contract", contractDeploy.Tv.String(), "err", err)
-		return nil, err
+	var err error
+	if !chain.IsZkSyncVM {
+		_, err = chain.Confirm(contractDeploy.Tx)
+		if err != nil {
+			lggr.Errorw("Failed to confirm deployment", "chain", chain.String(), "Contract", contractDeploy.Tv.String(), "err", err)
+			return nil, err
+		}
 	}
 	lggr.Infow("Deployed contract", "Contract", contractDeploy.Tv.String(), "addr", contractDeploy.Address, "chain", chain.String())
 	err = addressBook.Save(chain.Selector, contractDeploy.Address.String(), contractDeploy.Tv)
