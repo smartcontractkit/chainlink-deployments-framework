d843428ea145ac82eb3b1eae82ebb548a73f5b1c
cfal <cfal@users.noreply.github.com>
2025-05-21 15:08:51 +0400

    deployment/changeset_test.go: add SuiChain map argument

--- deployment/changeset_test.go
+++ deployment/changeset_test.go
@@ -106,6 +106,7 @@ func NewNoopEnvironment(t *testing.T) Environment {
 		map[uint64]Chain{},
 		map[uint64]SolChain{},
 		map[uint64]AptosChain{},
+		map[uint64]SuiChain{},
 		[]string{},
 		nil,
 		t.Context,
