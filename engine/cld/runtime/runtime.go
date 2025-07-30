689dd8829678517984e4868e4e1116f1ce7b0e66
James Kong <jkongie@gmail.com>
2025-07-30 14:02:48 +0800

    chore: adds directory structure for engine and experimental packages
    This adds placeholder directories for the `engine` and `experimental`
    packages. This structure is intended to support future development and
    organization of code related to the engine's runtime and experimental
    features.

---
+++ engine/cld/runtime/runtime.go
@@ -0,0 +1,4 @@
+// Package runtime is the runtime for the CLD engine. It is responsible for providing methods to load the Environment
+// and execute changesets using a predefined folder based approach called 'domains'.
+
+package runtime
---
+++ engine/test/runtime/runtime.go
@@ -0,0 +1,4 @@
+// Package runtime is the runtime for the test engine. It is responsible for providing methods to load the Environment
+// and execute changesets for unit and integration tests.
+
+package runtime
