ad6bf23773d8b79a0d43b8be1b0137bfd7b78619
James Kong <jkongie@gmail.com>
2025-07-30 16:39:25 +0800

    chore: adds directory structure for engine and experimental packages (#234)
    This adds placeholder directories for the `engine` and `experimental`
    packages. This structure is intended to support future development and
    organization of code related to the engine's runtime and experimental
    features.

---
+++ experimental/experimental.go
@@ -0,0 +1,4 @@
+// Package experimental is used to experiment with new features and ideas. Any code in this package is not guaranteed to be stable
+// or working, and may be change or removed at any time.
+
+package experimental
