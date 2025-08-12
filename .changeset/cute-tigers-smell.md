1d9675226efc16bff68b23772e70ceb6da962582
James Kong <jkongie@gmail.com>
2025-08-12 11:43:26 +0800

    refactor: refactor network url transformer (#259)
    This refactor changes the URL Transform Load Option to accept a simple
    transform function, and moves the logic of which URLs to transform to
    the config itself.
    
    This simplifies the caller's API and avoids exposing the internal config
    structure to the caller.

---
+++ .changeset/cute-tigers-smell.md
@@ -0,0 +1,5 @@
+---
+"chainlink-deployments-framework": minor
+---
+
+[BREAKING] The Load function of Network Config has been changed to simplify the URL transformation option
--- engine/cld/config/network/config.go
+++ engine/cld/config/network/config.go
@@ -134,6 +134,32 @@ func ChainFamilyFilter(chainFamily string) NetworkFilter {
 	}
 }
 
+// transformRPCURLs transforms the RPC URLs of the networks in the config.
+func (c *Config) transformRPCURLs(transform URLTransformer) {
+	for k, n := range c.networks {
+		// Transform the RPC URLS
+		for i, rpc := range n.RPCs {
+			rpc.HTTPURL = transform(rpc.HTTPURL)
+			rpc.WSURL = transform(rpc.WSURL)
+
+			n.RPCs[i] = rpc
+		}
+
+		// Transform EVM Metadata URLs
+		md, err := DecodeMetadata[EVMMetadata](n.Metadata)
+		if err != nil {
+			continue // skip this network since metadata cannot be decoded
+		}
+
+		md.AnvilConfig.ArchiveHTTPURL = transform(md.AnvilConfig.ArchiveHTTPURL)
+		n.Metadata = md
+
+		// Update the network with the modifications. We need to do this the network is a value
+		// type, so we need to update the map with the new network.
+		c.networks[k] = n
+	}
+}
+
 // Load loads configuration from the specified file paths, and merges them into a single Config.
 //
 // It accepts load options to customize the loading behavior.
@@ -162,8 +188,8 @@ func Load(filePaths []string, opts ...LoadOption) (*Config, error) {
 	}
 
 	// Apply the URL transformer if one is provided.
-	if loadCfg.URLTransformer != nil {
-		loadCfg.URLTransformer(cfg)
+	if loadCfg.RPCURLTransformer != nil {
+		cfg.transformRPCURLs(loadCfg.RPCURLTransformer)
 	}
 
 	return cfg, nil
@@ -172,17 +198,18 @@ func Load(filePaths []string, opts ...LoadOption) (*Config, error) {
 // LoadOption defines a function that modifies the load configuration.
 type LoadOption func(*loadConfig)
 
-// URLTransformer is a function that transforms the URLs in the Config.
-type URLTransformer func(*Config)
-
 // loadConfig holds the configuration for loading the config.
 type loadConfig struct {
-	URLTransformer URLTransformer
+	RPCURLTransformer URLTransformer
 }
 
-// WithURLTransform allows setting a custom URL transformer for the config.
-func WithURLTransform(t URLTransformer) func(opts *loadConfig) {
+// URLTransformer is a function that transforms a URL.
+type URLTransformer func(string) string
+
+// WithRPCURLTransformer is a load option that transforms the RPC URLs of the networks in the
+// config.
+func WithRPCURLTransformer(t URLTransformer) LoadOption {
 	return func(opts *loadConfig) {
-		opts.URLTransformer = t
+		opts.RPCURLTransformer = t
 	}
 }
