7ab96656631c6564e728d0823f5cd2d452aeee98
Graham Goh <graham.goh@smartcontract.com>
2025-08-28 00:27:19 +1000

    fix: remove chainlink-evm dep
    We imported github.com/smartcontractkit/chainlink-evm/gethwrappers in order to get ABI and BIN for the link token purely for testing, this is overkill. We can just copy the bin and abi over as it is just for testing and this allow us to remove that dependency.

--- chain/tron/provider/rpc_provider_test.go
+++ chain/tron/provider/rpc_provider_test.go
@@ -9,13 +9,13 @@ import (
 	"github.com/avast/retry-go/v4"
 	"github.com/fbsobreira/gotron-sdk/pkg/address"
 	chain_selectors "github.com/smartcontractkit/chain-selectors"
-	"github.com/smartcontractkit/chainlink-evm/gethwrappers/shared/generated/initial/link_token"
 	"github.com/smartcontractkit/chainlink-testing-framework/framework/components/blockchain"
 	"github.com/smartcontractkit/freeport"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-deployments-framework/chain/tron"
+	"github.com/smartcontractkit/chainlink-deployments-framework/chain/tron/provider/testdata"
 )
 
 func Test_RPCChainProviderConfig_validate(t *testing.T) {
@@ -262,7 +262,7 @@ func Test_Tron_SendTransfer_And_DeployContract(t *testing.T) {
 
 		// Deploy the LinkToken contract and wait for confirmation
 		contractAddress, txInfo, err := tronChain.DeployContractAndConfirm(
-			t.Context(), "LinkToken", link_token.LinkTokenABI, link_token.LinkTokenBin, nil, deployOptions)
+			t.Context(), "LinkToken", testdata.LinkTokenMetaDataABI, testdata.LinkTokenMetaDataBIN, nil, deployOptions)
 		require.NoError(t, err, "Failed to deploy contract")
 
 		// Log deployed contract address and deployment transaction details
