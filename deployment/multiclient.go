3232e8c26e8406eae6f6a795c119aea5223cb220
giogam <151543+giogam@users.noreply.github.com>
2025-09-04 09:43:46 +0200

    chore: type alias NewMultiClient

--- deployment/multiclient.go
+++ deployment/multiclient.go
@@ -1,8 +1,6 @@
 package deployment
 
 import (
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
-
 	"github.com/smartcontractkit/chainlink-deployments-framework/chain/evm/provider/rpcclient"
 )
 
@@ -12,6 +10,4 @@ type RetryConfig = rpcclient.RetryConfig
 type MultiClient = rpcclient.MultiClient
 
 // NewMultiClient creates a new MultiClient with failover capabilities.
-func NewMultiClient(lggr logger.Logger, rpcsCfg RPCConfig, opts ...func(client *MultiClient)) (*MultiClient, error) {
-	return rpcclient.NewMultiClient(lggr, rpcsCfg, opts...)
-}
+var NewMultiClient = rpcclient.NewMultiClient
