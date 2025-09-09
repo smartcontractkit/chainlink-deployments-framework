d400fed4149b6bb7f039b840e8ae879e589bbb61
Graham Goh <graham.goh@smartcontract.com>
2025-09-09 18:57:02 +1000

    [CLD-590]: docs: add godoc for exp and legacy pkg (#395)
    Documenting the purpose of experimental and legacy package. And added
    small intro on the operations package.
    
    JIRA: https://smartcontract-it.atlassian.net/browse/CLD-590

--- experimental/experimental.go
+++
@@ -1,4 +0,0 @@
-// Package experimental is used to experiment with new features and ideas. Any code in this package is not guaranteed to be stable
-// or working, and may be change or removed at any time.
-
-package experimental
