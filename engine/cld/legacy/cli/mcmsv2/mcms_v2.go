8673dc8094250a82f5b06e528e2189fffc658224
Joe Huang <joe.huang@smartcontract.com>
2025-11-04 09:47:13 -0600

    link jira

--- engine/cld/legacy/cli/mcmsv2/mcms_v2.go
+++ engine/cld/legacy/cli/mcmsv2/mcms_v2.go
@@ -1351,7 +1351,7 @@ func getExecutorWithChainOverride(cfg *cfgv2, chainSelector types.ChainSelector)
 			return nil, fmt.Errorf("error getting sui metadata from proposal: %w", err)
 		}
 		chain := cfg.blockchains.SuiChains()[uint64(chainSelector)]
-		entrypointEncoder := suibindings.NewCCIPEntrypointArgEncoder(metadata.RegistryObj, "") // FIXME: metadata current does not have deployer account obj
+		entrypointEncoder := suibindings.NewCCIPEntrypointArgEncoder(metadata.RegistryObj, "") // FIXME: metadata current does not have deployer account obj https://smartcontract-it.atlassian.net/browse/NONEVM-2954
 
 		return sui.NewExecutor(chain.Client, chain.Signer, encoder, entrypointEncoder, metadata.McmsPackageID, metadata.Role, cfg.timelockProposal.ChainMetadata[chainSelector].MCMAddress, metadata.AccountObj, metadata.RegistryObj, metadata.TimelockObj)
 	default:
@@ -1398,7 +1398,7 @@ func getTimelockExecutorWithChainOverride(cfg *cfgv2, chainSelector types.ChainS
 		if err != nil {
 			return nil, fmt.Errorf("error getting sui metadata from proposal: %w", err)
 		}
-		entrypointEncoder := suibindings.NewCCIPEntrypointArgEncoder(metadata.AccountObj, "") // FIXME: metadata current does not have deployer state obj
+		entrypointEncoder := suibindings.NewCCIPEntrypointArgEncoder(metadata.AccountObj, "") // FIXME: metadata current does not have deployer state obj https://smartcontract-it.atlassian.net/browse/NONEVM-2954
 		executor, err = sui.NewTimelockExecutor(chain.Client, chain.Signer, entrypointEncoder, metadata.McmsPackageID, metadata.RegistryObj, metadata.AccountObj)
 		if err != nil {
 			return nil, fmt.Errorf("error creating sui timelock executor: %w", err)
