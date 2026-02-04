828e221ad9d9f7eaa6263cb1533f4fc3923c68e3
Gustavo Gama <gustavo.gama@smartcontract.com>
2026-02-04 02:33:33 -0300

    [wip] renamed several types and added a building blocks of analyzer engine

---
+++ engine/cld/mcms/analyzer/internal/execution_context.go
@@ -0,0 +1,34 @@
+package internal
+
+import (
+	"github.com/smartcontractkit/chainlink-deployments-framework/chain"
+	"github.com/smartcontractkit/chainlink-deployments-framework/datastore"
+	cldfdomain "github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/domain"
+	"github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/mcms/analyzer"
+)
+
+// TODO: consider converting into simple struct type
+var _ analyzer.ExecutionContext = &executionContext{}
+
+type executionContext struct {
+	domain          cldfdomain.Domain
+	environmentName string
+	blockChains     chain.BlockChains
+	dataStore       datastore.DataStore
+}
+
+func (ec *executionContext) Domain() cldfdomain.Domain {
+	return ec.domain
+}
+
+func (ec *executionContext) EnvironmentName() string {
+	return ec.environmentName
+}
+
+func (ec *executionContext) BlockChains() chain.BlockChains {
+	return ec.blockChains
+}
+
+func (ec *executionContext) DataStore() datastore.DataStore {
+	return ec.dataStore
+}
