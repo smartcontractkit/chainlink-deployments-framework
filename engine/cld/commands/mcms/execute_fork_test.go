45f85870808b94d0d091f9ddcc6dee1563ea7b08
Pablo <pablo.estrada@smartcontract.com>
2026-02-19 11:26:17 -0600

    fix: lint errors

--- engine/cld/commands/mcms/execute_fork_test.go
+++ engine/cld/commands/mcms/execute_fork_test.go
@@ -308,7 +308,7 @@ func TestExecuteFork_OverrideForkChainDeployerKeyWithTestSigner(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		tc := tc
+
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			cfg, prevTxOpts := tc.setupCfg(t)
