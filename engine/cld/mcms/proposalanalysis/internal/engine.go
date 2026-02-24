01e5bbc67df20bbe2d251470daab508b73c6cc1f
Graham Goh <graham.goh@smartcontract.com>
2026-02-24 11:52:44 +1100

    feat(mcms/proposal): add skeleton for engine run implementation
    Update analyzer engine interfaces toward request-based execution and add option timeout tests to support the next step of proposal decoding and analysis orchestration work.

--- engine/cld/mcms/proposalanalysis/internal/engine.go
+++ engine/cld/mcms/proposalanalysis/internal/engine.go
@@ -2,6 +2,8 @@ package proposalanalysis
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"io"
 
 	"github.com/smartcontractkit/mcms"
@@ -9,15 +11,93 @@ import (
 	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	cldfdomain "github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/domain"
 	"github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/mcms/proposalanalysis/internal/analyzer"
+	"github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/mcms/proposalanalysis/internal/decoder"
 	"github.com/smartcontractkit/chainlink-deployments-framework/engine/cld/mcms/proposalanalysis/internal/renderer"
 )
 
+type RunRequest struct {
+	Domain        cldfdomain.Domain
+	Environment   *deployment.Environment
+	DecoderConfig decoder.Config
+}
+
 type AnalyzerEngine interface {
-	Run(ctx context.Context, domain cldfdomain.Domain, env deployment.Environment, proposal *mcms.TimelockProposal) (analyzer.AnalyzedProposal, error)
+	Run(ctx context.Context, req RunRequest, proposal *mcms.TimelockProposal) (analyzer.AnalyzedProposal, error)
 
 	RegisterAnalyzer(analyzer analyzer.BaseAnalyzer) error
 
 	RegisterRenderer(renderer renderer.Renderer) error
 
-	RenderTo(w io.Writer, rendererID string, proposal analyzer.AnalyzedProposal) error
+	RenderTo(w io.Writer, rendererID string, renderReq renderer.RenderRequest, proposal analyzer.AnalyzedProposal) error
+}
+
+type Deps struct {
+	DecoderFactory func(cfg decoder.Config) (decoder.ProposalDecoder, error)
+}
+
+func defaultDeps() Deps {
+	return Deps{
+		// DecoderFactory: decoder.New,
+	}
+}
+
+type analyzerEngine struct {
+	analyzerRegistry *analyzer.Registry
+	rendererRegistry *renderer.Registry
+	config           *engineConfig
+	deps             Deps
+}
+
+func NewAnalyzerEngine(opts ...EngineOption) AnalyzerEngine {
+	return newAnalyzerEngineWithDeps(Deps{}, opts...)
+}
+
+func newAnalyzerEngineWithDeps(deps Deps, opts ...EngineOption) AnalyzerEngine {
+	cfg := ApplyEngineOptions(opts...)
+	resolvedDeps := defaultDeps()
+	if deps.DecoderFactory != nil {
+		resolvedDeps.DecoderFactory = deps.DecoderFactory
+	}
+
+	return &analyzerEngine{
+		analyzerRegistry: analyzer.NewRegistry(),
+		rendererRegistry: renderer.NewRegistry(),
+		config:           cfg,
+		deps:             resolvedDeps,
+	}
+}
+
+func (e *analyzerEngine) RegisterAnalyzer(a analyzer.BaseAnalyzer) error {
+	return e.analyzerRegistry.Register(a)
+}
+
+func (e *analyzerEngine) RegisterRenderer(r renderer.Renderer) error {
+	return e.rendererRegistry.Register(r)
+}
+
+func (e *analyzerEngine) Run(ctx context.Context, req RunRequest, proposal *mcms.TimelockProposal) (analyzer.AnalyzedProposal, error) {
+	if proposal == nil {
+		return nil, errors.New("proposal cannot be nil")
+	}
+	if req.Domain.Key() == "" {
+		return nil, errors.New("domain cannot be empty")
+	}
+	if req.Environment == nil {
+		return nil, errors.New("environment cannot be nil")
+	}
+
+	// todo
+	// decode
+	// analyze
+
+	return nil, errors.New("not implemented")
+}
+
+func (e *analyzerEngine) RenderTo(w io.Writer, rendererID string, renderReq renderer.RenderRequest, proposal analyzer.AnalyzedProposal) error {
+	r, ok := e.rendererRegistry.Get(rendererID)
+	if !ok {
+		return fmt.Errorf("renderer with ID %q is not registered", rendererID)
+	}
+
+	return r.RenderTo(w, renderReq, proposal)
 }
