5d46212481a959863df497758af3f9ac7581e849
Finley Decker <finley.decker@smartcontract.com>
2026-03-16 16:54:15 -1000

    revert prealloc change in lanedetector analyzer

--- engine/cld/mcms/proposalanalysis/examples/ccip/analyzers/lanedetector/analyzer.go
+++ engine/cld/mcms/proposalanalysis/examples/ccip/analyzers/lanedetector/analyzer.go
@@ -67,7 +67,7 @@ func (a *LaneDetectorAnalyzer) Analyze(
 
 	// Find bidirectional pairs: A>B and B>A both exist.
 	seen := make(map[edge]struct{})
-	lanes := make([]string, 0, len(edgeSet))
+	var lanes []string
 
 	for e := range edgeSet {
 		if _, ok := edgeSet[edge{e.to, e.from}]; !ok {
