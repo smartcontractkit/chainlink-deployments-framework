f02258550f32a4634e06f4697bce83cc75b4fa0b
Pablo <pablo.estrada@smartcontract.com>
2026-03-25 16:54:52 -0600

    fix: lint errors

--- cre/artifacts/workdir.go
+++ cre/artifacts/workdir.go
@@ -2,18 +2,13 @@ package artifacts
 
 import (
 	"crypto/rand"
-	"encoding/binary"
 	"encoding/hex"
-	mathrand "math/rand/v2"
 )
 
 // randomWorkDirSuffix returns exactly 16 hex chars (8 random bytes) for unique artifact filenames.
-// Falls back to math/rand if the OS CSPRNG fails, which is weaker but sufficient for filename uniqueness.
 func randomWorkDirSuffix() string {
 	var b [8]byte
-	if _, err := rand.Read(b[:]); err != nil {
-		binary.BigEndian.PutUint64(b[:], mathrand.Uint64())
-	}
+	_, _ = rand.Read(b[:])
 
 	return hex.EncodeToString(b[:])
 }
