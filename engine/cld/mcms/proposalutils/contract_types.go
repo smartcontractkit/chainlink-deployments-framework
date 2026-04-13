92973244dbb350616f60a20e1a1ef728014d6e27
Pablo <pablo.estrada@smartcontract.com>
2026-04-13 14:18:12 -0600

    fix: move helpers to cld/mcms. mark old types as deprecated

---
+++ engine/cld/mcms/proposalutils/contract_types.go
@@ -0,0 +1,19 @@
+package proposalutils
+
+import cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
+
+const (
+	BypasserManyChainMultisig        cldf.ContractType = "BypasserManyChainMultiSig"
+	CancellerManyChainMultisig       cldf.ContractType = "CancellerManyChainMultiSig"
+	ProposerManyChainMultisig        cldf.ContractType = "ProposerManyChainMultiSig"
+	ManyChainMultisig                cldf.ContractType = "ManyChainMultiSig"
+	RBACTimelock                     cldf.ContractType = "RBACTimelock"
+	CallProxy                        cldf.ContractType = "CallProxy"
+	ManyChainMultisigProgram         cldf.ContractType = "ManyChainMultiSigProgram"
+	RBACTimelockProgram              cldf.ContractType = "RBACTimelockProgram"
+	AccessControllerProgram          cldf.ContractType = "AccessControllerProgram"
+	ProposerAccessControllerAccount  cldf.ContractType = "ProposerAccessControllerAccount"
+	ExecutorAccessControllerAccount  cldf.ContractType = "ExecutorAccessControllerAccount"
+	CancellerAccessControllerAccount cldf.ContractType = "CancellerAccessControllerAccount"
+	BypasserAccessControllerAccount  cldf.ContractType = "BypasserAccessControllerAccount"
+)
--- engine/cld/mcms/proposalutils/types.go
+++ engine/cld/mcms/proposalutils/types.go
@@ -5,38 +5,10 @@ import (
 
 	"github.com/smartcontractkit/ccip-owner-contracts/pkg/config"
 	mcmstypes "github.com/smartcontractkit/mcms/types"
-
-	cldf "github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 )
 
-const (
-	BypasserManyChainMultisig  cldf.ContractType = "BypasserManyChainMultiSig"
-	CancellerManyChainMultisig cldf.ContractType = "CancellerManyChainMultiSig"
-	ProposerManyChainMultisig  cldf.ContractType = "ProposerManyChainMultiSig"
-	ManyChainMultisig          cldf.ContractType = "ManyChainMultiSig"
-	RBACTimelock               cldf.ContractType = "RBACTimelock"
-	CallProxy                  cldf.ContractType = "CallProxy"
-
-	// LinkToken is the burn/mint link token. It should be used everywhere for
-	// new deployments. Corresponds to
-	// https://github.com/smartcontractkit/chainlink/blob/develop/core/gethwrappers/shared/generated/link_token/link_token.go#L34
-	LinkToken cldf.ContractType = "LinkToken"
-	// StaticLinkToken represents the (very old) non-burn/mint link token.
-	// It is not used in new deployments, but still exists on some chains
-	// and has a distinct ABI from the new LinkToken.
-	// Corresponds to the ABI
-	// https://github.com/smartcontractkit/chainlink/blob/develop/core/gethwrappers/generated/link_token_interface/link_token_interface.go#L34
-	StaticLinkToken cldf.ContractType = "StaticLinkToken"
-	// mcms Solana specific
-	ManyChainMultisigProgram         cldf.ContractType = "ManyChainMultiSigProgram"
-	RBACTimelockProgram              cldf.ContractType = "RBACTimelockProgram"
-	AccessControllerProgram          cldf.ContractType = "AccessControllerProgram"
-	ProposerAccessControllerAccount  cldf.ContractType = "ProposerAccessControllerAccount"
-	ExecutorAccessControllerAccount  cldf.ContractType = "ExecutorAccessControllerAccount"
-	CancellerAccessControllerAccount cldf.ContractType = "CancellerAccessControllerAccount"
-	BypasserAccessControllerAccount  cldf.ContractType = "BypasserAccessControllerAccount"
-)
-
+// DEPRECATED: use MCMSWithTimelockConfig
+// this will removed once all ccip code is migrated to use the new MCMSWithTimelockConfig type.
 type MCMSWithTimelockConfigLegacy struct {
 	Canceller        config.Config `json:"canceller"`
 	Bypasser         config.Config `json:"bypasser"`
