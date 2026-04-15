3644736feecc276b191831db5befe695e3a59bcf
Pablo <pablo.estrada@smartcontract.com>
2026-04-14 18:31:24 -0600

    fix: add missing aptos helper, it is still needed to be able to keep backwards compatibility

---
+++ engine/cld/mcms/proposalutils/aptos_utils.go
@@ -0,0 +1,13 @@
+package proposalutils
+
+import (
+	mcmsaptossdk "github.com/smartcontractkit/mcms/sdk/aptos"
+	mcmstypes "github.com/smartcontractkit/mcms/types"
+)
+
+func GetAptosRoleFromAction(action mcmstypes.TimelockAction) (mcmsaptossdk.TimelockRole, error) {
+	if action == "" {
+		return mcmsaptossdk.TimelockRoleProposer, nil
+	}
+	return mcmsaptossdk.AptosRoleFromAction(action)
+}
